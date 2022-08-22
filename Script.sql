fbd481d98ef052ec412f1f0920b57c1df5a1857c
lausegreda <92129634+lausegreda@users.noreply.github.com>
2022-08-21 22:11:05 -0600

    Update Script.sql
    Se crea tabla menu con los productos que se pueden comprar

--- Script.sql
+++ Script.sql
@@ -61,3 +61,65 @@ CREATE TABLE `restaurante`.`carrito` (`id_carrito` INT NOT NULL AUTO_INCREMENT, 
 foreign key fk_carrito_cliente (id_cliente) references cliente(id_cliente) )
 ENGINE = innoDB;
 
+CREATE TABLE `restaurante`.`menu` 
+(
+  id_platillo INT NOT NULL AUTO_INCREMENT,
+  descripcion VARCHAR(70) NOT NULL,  
+  categoria VARCHAR(30) NOT NULL,
+  precio double,
+  PRIMARY KEY (id_platillo)
+)
+ENGINE = InnoDB;
+
+INSERT INTO restaurante.menu(descripcion, categoria, precio)
+VALUES 
+('Trio de Ceviches', 'Entradas', '6950'),
+('Ceviche de Pescado', 'Entradas', '4700'),
+('Ceviche Costeño', 'Entradas', '5390'),
+('Ceviche de Camarón', 'Entradas', '5250'),
+('Ceviche Mixto', 'Entradas', '4300'),
+('Canasta de Patacones', 'Entradas', '3900'),
+('Entrada de la Casa', 'Entradas', '3900'),
+('Carpaccio de Res', 'Entradas', '8900'),
+('Carpaccio de Salmón', 'Entradas', '11900'),
+('Ensalada Especial del Chef', 'Entradas', '4600'),
+('Ensalada con Salmón', 'Entradas', '5300'),
+('Sopa de Pollo', 'Entradas', '5990'),
+('Sopa de Mariscos', 'Entradas', '6600'),
+('Olla de Carne pequeña', 'Entradas', '5200'),
+('Parrillada Campesina (2 Personas)', 'Principal', '21900'),
+('Parrillada campesina (4 personas)', 'Principal', '39900'),
+('Parrillada Campesina Delux (4 personas)', 'Principal', '49900'),
+('Surtido Criollo (2 personas)', 'Principal', '21900'),
+('Surtido Criollo (4 personas)', 'Principal', '39900'),
+('Parrillada de Mariscos (2 personas)', 'Principal', '21900'),
+('Parrillada de Mariscos (4 personas)', 'Principal', '39900'),
+('Canasta de chicharrones', 'Principal', '27900'),
+('Mar y Tierra', 'Principal', '15400'),
+('Lomito de Res', 'Principal', '13250'),
+('Rib Eye', 'Principal', '15800'),
+('Churrasco', 'Principal', '16900'),
+('T-Bone', 'Principal', '17900'),
+('Filet a la Calipso', 'Principal', '14900'),
+('Salmón Grillado', 'Principal', '13400'),
+('Pescado Entero Frito', 'Principal', '10600'),
+('Pulpo Caribeño', 'Principal', '13900'),
+('Arroz con Camarones', 'Principal', '6900'),
+('Arroz con Pollo', 'Principal', '6200'),
+('Rice ans Beans', 'Principal', '6900'),
+('Casado Típico', 'Principal', '6400'),
+('Chifrijo', 'Principal', '6750'),
+('Meringatta de Frutos Rojos', 'Postres', '4100'),
+('Torta de Crema Italiana', 'Postres', '4500'),
+('Tiramisú', 'Postres', '3900'),
+('Cazuela de Galleta', 'Postres', '4800'),
+('Torta Capresse', 'Postres', '4500'),
+('Refresco Natural', 'Bebidas', '2500'),
+('Gaseosos', 'Bebidas', '1500'),
+('Botella de Agua', 'Bebidas', '1000'),
+('Cervezas', 'Bebidas', '2500'),
+('Cocteles', 'Bebidas', '4500'),
+('Nuggets con papas', 'Infantil', '3500'),
+('Hamburguesa con papas', 'Infantil', '4500'),
+('Pizza personal', 'Infantil', '5500');
+
