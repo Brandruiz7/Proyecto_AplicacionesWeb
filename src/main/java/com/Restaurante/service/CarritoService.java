955dab0966d59746edbad90199c2919facf0eae1
GhostFix7 <brandruiz7@gmail.com>
2022-08-22 18:34:09 -0600

    Actualización: Cambios importantes
    Cambios de Camila por error no se pudo subir desde su máquina.

--- src/main/java/com/Restaurante/service/CarritoService.java
+++
@@ -1,9 +0,0 @@
-package com.Restaurante.service;
-
-import com.Restaurante.domain.Cliente;
-import java.util.List; 
-
-public interface CarritoService {
-    public Carrito getCarrito(Carrito carrito);
-    public Carrito getCarritoCliente(long idCliente);
-}
